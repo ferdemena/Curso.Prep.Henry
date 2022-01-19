010127d1f658078bbf969b4d7ff46ee55c23420d
fernanda de Mena <ferdemena@gmail.com>
2022-01-18 23:23:46 -0300

    quinto cambio

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -85,14 +85,20 @@ function menosQueNoventa(num) {
   // Devuelve "true" si el argumento de la función "num" es menor que noventa
   // De lo contrario, devuelve "false"
   // Tu código:
-  
+    if (num < 90) {
+      return true; 
+    }
+    return false
 }
 
 function mayorQueCincuenta(num) {
   // Devuelve "true" si el argumento de la función "num" es mayor que cincuenta
   // De lo contrario, devuelve "false"
   // Tu código:
-  
+    if (num>50) {
+      return true;
+    }
+    return false
 }
 
 function obtenerResto(x, y) {
