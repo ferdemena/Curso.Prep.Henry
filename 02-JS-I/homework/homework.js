1f553f326b228cc1591ff9778a5181fba81bdcb4
fernanda de Mena <ferdemena@gmail.com>
2022-01-19 12:14:55 -0300

    sexto cambio

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -112,16 +112,22 @@ function esPar(num) {
   // Devuelve "true" si "num" es par
   // De lo contrario, devuelve "false"
   // Tu código:
-  
+     if (num % 2 === 0) {
+       return true;
+     } else {
+     return false
 }
-
+}
 function esImpar(num) {
   // Devuelve "true" si "num" es impar
   // De lo contrario, devuelve "false"
   // Tu código:
-  
+    if (num % 2 === 1) {
+      return true;
+    } else {
+    return false
 }
-
+}
 function elevarAlCuadrado(num) {
   // Devuelve el valor de "num" elevado al cuadrado
   // ojo: No es raiz cuadrada!
