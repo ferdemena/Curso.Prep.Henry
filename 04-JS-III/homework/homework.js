d0679d11ac224100bce88ba9001035121a3d2c7c
fernanda de Mena <ferdemena@gmail.com>
2022-02-08 19:12:24 -0300

    04 JS III L110 passed 12

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -10,7 +10,7 @@ function devolverPrimerElemento(array) {
 function devolverUltimoElemento(array) {
   // Devuelve el último elemento de un array
   // Tu código:
-  return array [array.length - 1];
+  return array.pop ();
 }
 
 
@@ -36,7 +36,7 @@ function agregarItemAlFinalDelArray(array, elemento) {
   // Añade el "elemento" al final del array
   // y devuelve el array
   // Tu código:
-  
+
   array.push (elemento);
   return array;
 }
@@ -47,6 +47,8 @@ function agregarItemAlComienzoDelArray(array, elemento) {
   // y devuelve el array
   // Pista: usa el método `.unshift`
   // Tu código:
+  array.unshift (elemento);
+  return array;
 }
 
 
@@ -56,20 +58,30 @@ function dePalabrasAFrase(palabras) {
   // con espacios entre cada palabra
   // Ejemplo: ['Hello', 'world!'] -> 'Hello world!'
   // Tu código:
-}
-
+  return palabras.join (' ')
+  }
 
 function arrayContiene(array, elemento) {
   // Comprueba si el elemento existe dentro de "array"
   // Devuelve "true" si está, o "false" si no está
   // Tu código:
+  for (let i=0; i< array.length; i++) {
+    if (array [i] === elemento) {
+    return true;
+    }
+  } 
+  return false;
 }
-
 
 function agregarNumeros(numeros) {
   // "numeros" debe ser un arreglo de enteros (int/integers)
   // Suma todos los enteros y devuelve el valor
   // Tu código:
+  var sumaa = 0
+  for (let i=0;i< numeros.length;i++) {
+    sumaa= sumaa + numeros [i];
+  }
+  return sumaa;
 }
 
 
@@ -77,6 +89,7 @@ function promedioResultadosTest(resultadosTest) {
   // "resultadosTest" debe ser una matriz de enteros (int/integers)
   // Itera (en un bucle) los elementos del array, calcula y devuelve el promedio de puntajes
   // Tu código:
+  return agregarNumeros (resultadosTest)/resultadosTest.length;
 }
 
 
@@ -84,13 +97,28 @@ function numeroMasGrande(numeros) {
   // "numeros" debe ser una matriz de enteros (int/integers)
   // Devuelve el número más grande
   // Tu código:
-}
+  var mayor = numeros [0]
+  for (let i=1; i< numeros.length; i++) {
+    if (numeros [i] > mayor) {
+      mayor = numeros [i];
+    }
+  }
+  return mayor;
+  }
 
 
 function multiplicarArgumentos() {
   // Usa la palabra clave `arguments` para multiplicar todos los argumentos y devolver el producto
   // Si no se pasan argumentos devuelve 0. Si se pasa un argumento, simplemente devuélvelo
   // Escribe tu código aquí:
+
+  if (arguments.length === 0) return 0;
+
+    var producto = 1
+    for (let i=0;i< arguments.length;i++) {
+      producto*= arguments [i];
+    }
+  return producto;
 }
 
 
